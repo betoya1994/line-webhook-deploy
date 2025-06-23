cda3aa9c8d50dbc46831a6cb9f9e8fd31a40cede
betoya1994 <tranduchoang240994@gmail.com>
2025-06-24 06:49:52 +0900

    server.js for LINE webhook

---
+++ server.js
@@ -0,0 +1,39 @@
+const express = require('express');
+const { middleware, Client } = require('@line/bot-sdk');
+const bodyParser = require('body-parser');
+require('dotenv').config();
+
+const config = {
+  channelAccessToken: process.env.CHANNEL_ACCESS_TOKEN,
+  channelSecret: process.env.CHANNEL_SECRET
+};
+
+const client = new Client(config);
+const app = express();
+
+app.use(middleware(config));
+app.use(bodyParser.json());
+
+app.post('/webhook', (req, res) => {
+  Promise.all(req.body.events.map(handleEvent))
+    .then(result => res.json(result))
+    .catch(err => {
+      console.error(err);
+      res.status(500).end();
+    });
+});
+
+function handleEvent(event) {
+  if (event.type !== 'message' || event.message.type !== 'text') {
+    return Promise.resolve(null);
+  }
+
+  return client.replyMessage(event.replyToken, {
+    type: 'text',
+    text: 'Cảm ơn bạn đã nhắn: ' + event.message.text
+  });
+}
+
+app.listen(3000, () => {
+  console.log('Server is running');
+});
